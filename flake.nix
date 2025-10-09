9a8f4bc10de23833f36791015ef49f684aed63b0
aSymphonic <ratatosk20@gmail.com>
2025-10-08 18:25:33 -0700

    first flake

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "A NixOS flake that includes all packages from configuration.nix";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; # Use a specific branch or commit
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux"; # or detect automatically using builtins.currentSystem
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
