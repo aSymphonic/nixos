2c47c3b5e3e6ca2e72d6c21081898eea75ebadf0
aSymphonic <ratatosk20@gmail.com>
2025-10-21 19:09:17 -0700

    modified:   configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,4 +1,5 @@
 # Edit this configuration file to define what should be installed on
+
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
@@ -105,6 +106,7 @@
   environment.systemPackages = with pkgs; [
     pkgs.neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     pkgs.firefox
+	pkgs.wofi
     pkgs.python314
     pkgs.libgccjit
     pkgs.ripgrep
